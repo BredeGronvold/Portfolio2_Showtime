0eeac25322da69658ebb7bb44e1efdf32db39808
s345871 <s345871@oslomet.no>
2021-04-24 19:29:54 +0200

    done for today

--- Portfolio2_Showtime/src/main/java/com/example/Portfolio2_Showtime/item/ItemServer.java
+++ Portfolio2_Showtime/src/main/java/com/example/Portfolio2_Showtime/item/ItemServer.java
@@ -9,12 +9,12 @@ import java.util.Arrays;
 @Service
 public class ItemServer {
     private static final ArrayList<Item> savedItems = new ArrayList<>(Arrays.asList(
-            new Item(setID(), "Volleyball", 5, "https://freepngimg.com/thumb/volleyball/28621-8-volleyball-free-download-thumb.png", "short description", "long description"),
-            new Item(setID(), "Fotball", 5, "https://www.premierleague.com/resources/prod/e1fd15c-3351/i/nike-ball-hub/index/21.png", "short description", "long description"),
-            new Item(setID(), "Basketball", 5, "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/NEBL-Spalding-basket-ball.png/769px-NEBL-Spalding-basket-ball.png", "short description", "long description"),
-            new Item(setID(), "Hockey puck", 5, "https://pngimg.com/uploads/hockey/hockey_PNG95.png", "short description", "long description"),
-            new Item(setID(), "Tennisball", 5, "https://assets.stickpng.com/images/580b585b2edbce24c47b2b90.png", "short description", "long description"),
-            new Item(setID(), "Amerikansk fotball", 5, "https://www.pngkey.com/png/full/166-1664900_american-football-transparent-background-png-american-football-ball.png", "short description", "long description")
+            new Item(setIDFirst(), "Volleyball", 5, "https://freepngimg.com/thumb/volleyball/28621-8-volleyball-free-download-thumb.png", "short description", "long description"),
+            new Item(setIDFirst(), "Fotball", 5, "https://www.premierleague.com/resources/prod/e1fd15c-3351/i/nike-ball-hub/index/21.png", "short description", "long description"),
+            new Item(setIDFirst(), "Basketball", 5, "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/NEBL-Spalding-basket-ball.png/769px-NEBL-Spalding-basket-ball.png", "short description", "long description"),
+            new Item(setIDFirst(), "Hockey puck", 5, "https://pngimg.com/uploads/hockey/hockey_PNG95.png", "short description", "long description"),
+            new Item(setIDFirst(), "Tennisball", 5, "https://assets.stickpng.com/images/580b585b2edbce24c47b2b90.png", "short description", "long description"),
+            new Item(setIDFirst(), "Amerikansk fotball", 5, "https://www.pngkey.com/png/full/166-1664900_american-football-transparent-background-png-american-football-ball.png", "short description", "long description")
     ));
 
 
@@ -57,15 +57,16 @@ public class ItemServer {
 
     private static int setID() {
         int number = (int) (Math.random() * 100000) + 100000;
-        /*if(!savedItems.isEmpty()) {
-            for (Item element : savedItems) {
-                if (element.getId() == number) {
-                    setID();
-                }
+        for (Item element : savedItems) {
+            if (element.getId() == number) {
+                setID();
             }
-        }*/
-
+        }
         return number;
+    }
+
+    private static int setIDFirst() {
+        return (int) (Math.random() * 100000) + 100000;
     }
 
     public Item setPic(int item_id, String url) {
