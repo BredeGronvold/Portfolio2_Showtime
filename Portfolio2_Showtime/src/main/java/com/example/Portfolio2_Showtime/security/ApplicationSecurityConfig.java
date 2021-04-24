bdefa67baae9bc77cea06d6e0e5e39232f1ea044
DaHenke <henkem212@gmail.com>
2021-04-24 13:37:51 +0200

    Added permit for /home and /api/* in ApplicationSecurityConfig

--- Portfolio2_Showtime/src/main/java/com/example/Portfolio2_Showtime/security/ApplicationSecurityConfig.java
+++ Portfolio2_Showtime/src/main/java/com/example/Portfolio2_Showtime/security/ApplicationSecurityConfig.java
@@ -1,6 +1,8 @@
-/*
+
 package com.example.Portfolio2_Showtime.security;
 
+import com.example.Portfolio2_Showtime.item.ItemController;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,14 +11,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/","index","/css/*","/js/*","/home","/api/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
+                .csrf().disable()
                 .httpBasic();
+
     }
 }
-*/
+
